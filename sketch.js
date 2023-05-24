b4f1055605a858606c4a89682bafa61501e71b06
Jordan Graves <jordan@jordangrav.es>
2023-05-24 12:25:10 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function scroll1(a)
+{
+    console.log(a);
+    let element= document.getElementById(a);
+    element.scrollTo();
+    
+}
+
+/*
+setTimeout(function() {
+
+   let d= document.getElementsByClassName("MagicScroll");
+
+   for (let i=0; i<d.length; i++) {
+d[i].style.height="500px";
+//d[i].style.position="500px";
+   }
+ },2000);
+ */
+
+ 
